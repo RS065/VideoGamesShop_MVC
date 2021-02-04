649f650055d0592e8b51685baebc3d7db320fe02
Ranjodh <Ranjodh @gmail.com>
2021-02-05 00:14:01 +1300

    Add project files.

---
+++ Migrations/VideoGamesShop_MVCData/20210204110846_Video.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VideoGamesShop_MVC.Migrations.VideoGamesShop_MVCData
+{
+    public partial class Video : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Player",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ContactNumber = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Player", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VideoGame",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    HighestScore = table.Column<int>(type: "int", nullable: false),
+                    PricePerPlay = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VideoGame", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GamePlay",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PlayerId = table.Column<int>(type: "int", nullable: false),
+                    VideoGameId = table.Column<int>(type: "int", nullable: false),
+                    StartTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GamePlay", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_GamePlay_Player_PlayerId",
+                        column: x => x.PlayerId,
+                        principalTable: "Player",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_GamePlay_VideoGame_VideoGameId",
+                        column: x => x.VideoGameId,
+                        principalTable: "VideoGame",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GamePlay_PlayerId",
+                table: "GamePlay",
+                column: "PlayerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GamePlay_VideoGameId",
+                table: "GamePlay",
+                column: "VideoGameId");
+            var sqlFile = Path.Combine(".\\DatabaseScripts", @"data.sql");
+
+            migrationBuilder.Sql(File.ReadAllText(sqlFile));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GamePlay");
+
+            migrationBuilder.DropTable(
+                name: "Player");
+
+            migrationBuilder.DropTable(
+                name: "VideoGame");
+        }
+    }
+}
